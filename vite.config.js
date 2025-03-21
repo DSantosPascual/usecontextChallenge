6f207dc3ba5530f5ef82eadb287a8c729fb5c228
DSantosPascual <d.santos.pascual@gmail.com>
2025-03-21 17:39:52 +0100

    jsx

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  esbuild: {
+    loader: 'jsx',
+    include: /src\/.*\.js$/, // Permite JSX en archivos .js dentro de /src
+  },
+});
